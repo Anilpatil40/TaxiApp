7908688393888b82453d1f48c7ffdbb8838e1268
panil8993 <panil8993@gmail.com>
2021-01-16 12:53:48 +0530

    designing of app.

--- app/src/main/java/com/swayam/taxiapp/FirstFragment.java
+++ app/src/main/java/com/swayam/taxiapp/FirstFragment.java
@@ -20,6 +20,6 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        
+
     }
 }
